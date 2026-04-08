5552f9843f3416210ab28305561a5fedb9841661
Muad-syntax <mohammad.suaebummuad9smk.belajar.id>
2026-04-08 18:39:49 +0700

    update aray & object

--- aray/aray.js
+++ aray/aray.js
@@ -5,3 +5,19 @@ for(let i = 0; i < jurusan.length; i++){
     document.writeln(`<li>${jurusan[i]}</li>`)
 }
 document.writeln("</ol>")
+
+let dataSiswa = {
+    namaDepan: "Ahmad",
+    namaBelakang: "Maliki",
+    jurusan: jurusan[1],
+    kelas: "XI",
+    umur: 18,
+    nilaiAkademik: 95
+}
+document.writeln("<h3>Data Siswa</h3>")
+document.writeln(`<b>Nama Depan: </b>${dataSiswa.namaDepan}`)
+document.writeln(`<br><b>Nama Belakang: </b>${dataSiswa.namaBelakang}`)
+document.writeln(`<br><b>Jurusan: </b>${dataSiswa.jurusan}`)
+document.writeln(`<br><b>Kelas: </b>${dataSiswa.kelas}`)
+document.writeln(`<br><b>Umur: </b>${dataSiswa.umur}`)
+document.writeln(`<br><b>Nilai Akademik: </b>${dataSiswa.nilaiAkademik}`)
