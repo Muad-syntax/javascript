35f240163315e7cc7a71459ae7e93d2b0902ef32
Muad-syntax <mohammad.suaebummuad9smk.belajar.id>
2026-04-13 21:11:41 +0700

    update lagi

--- juragan angkot/angkot1.js
+++ juragan angkot/angkot1.js
@@ -1,6 +1,6 @@
 var jmlhAngkot = 25;
 var angkot = 1;
 while(angkot <= jmlhAngkot){
-    console.log('Angkot No. ' + angkot + ' beroperasi dengan baik.')
+    document.write('Angkot No. ' + angkot + ' beroperasi dengan baik.<br>')
 angkot++;
 }
